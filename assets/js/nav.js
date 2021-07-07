32df712d5151ce1a81f2ef9fa5eece7fd7330cff
Ahmet ARVAS <71208337+Ahmet-Arvas@users.noreply.github.com>
2021-07-07 17:46:58 +0300

    update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -4,3 +4,10 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+document.addEventListener('DOMContentLoaded', function() {
+  var sticky =
+      !!getComputedStyle(document.querySelector('main')).webkitOverflowScrolling;
+  if(sticky) {
+      document.body.className = "sticky";
+  }
+});
